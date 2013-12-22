c26f8fe807dc4638af93a95b6fcb8695bc995e15
gye-tgm <gye@student.tgm.ac.at>
2013-12-22 14:41:46 +0100

    client version in java

--- client/src/schrackye/Client.java
+++ client/src/schrackye/Client.java
@@ -1,10 +1,34 @@
 package schrackye;
 
+import java.util.Properties;
+
 import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.InvalidName;
 
 public class Client {
-
+	private static String id = "Echo";
+	private static String kind = "Object";
+	
 	public static void main(String[] args){
+		Echo echo;
+		try{
+			// get a reference to the naming service
+			org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
+			org.omg.CORBA.Object o = orb.resolve_initial_references("NameService");
 		
+			NamingContextExt rootContext = NamingContextExtHelper.narrow( o );
+			
+			NameComponent[] name = new NameComponent[2];
+			name[0] = new NameComponent("test","my_context");
+			name[1] = new NameComponent(id, kind);
+		
+			echo = EchoHelper.narrow(rootContext.resolve(name));
+			System.out.println("Server says : " + echo.echoString("Omgosh"));
+		} catch(InvalidName e){
+			System.err.println("InvalidNameException occured : " + e.getMessage());
+		} catch(Exception e){
+			System.err.println("Unknown exception occured : " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
